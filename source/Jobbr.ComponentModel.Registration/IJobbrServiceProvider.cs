f2c5a4ef1ce0c3c645d5945f3edbd251ae803602
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-01-19 21:11:57 +0100

    added service provider

---
+++ source/Jobbr.ComponentModel.Registration/IJobbrServiceProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jobbr.ComponentModel.Registration
+{
+    public interface IJobbrServiceProvider
+    {
+        object GetService(Type serviceType);
+
+        IEnumerable<object> GetServices(Type serviceType);
+
+        IJobbrServiceProvider GetChild();
+    }
+}
