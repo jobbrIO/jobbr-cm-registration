bc212b24c518bbc14741d43b57f9680449f66417
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-01-25 14:28:19 +0100

    added type to identity configuration classes

---
+++ source/Jobbr.ComponentModel.Registration/IFeatureConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace Jobbr.ComponentModel.Registration
+{
+    /// <summary>
+    /// Indicates a class that contains specific configuration settings for a given feature
+    /// </summary>
+    public interface IFeatureConfiguration
+    {
+    }
+}
